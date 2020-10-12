81a5ac98a109c08533fbdb0382c846157934c2aa
Ryan <rmc7394@gmail.com>
2020-10-12 16:28:53 +0100

    Edit #2 GameBoard class
    Tried to test generating num of rows and columns from user input, defaulting to displaying 0 after adding getters and setters as opposed to calling variable directly.

--- GameBoard.java
+++ GameBoard.java
@@ -4,31 +4,33 @@ import java.util.ArrayList;
 public class GameBoard {
 
     private String name;
-    // Generate arraylists with the num of rows and columns from user input
-    private ArrayList<Row> rows = new ArrayList<Row>(numOfRows());
-    private ArrayList<Column> columns = new ArrayList<Column>(numOfColumns());
+    private int numOfRows;
+    private int numOfColumns;
+    // Generate arraylists with the num of rows and columns from method
+    private ArrayList<Row> rows = new ArrayList<Row>();
+    private ArrayList<Column> columns = new ArrayList<Column>();
+
 
     //Constructor
     public GameBoard(String name) {
         this.name = name;
     }
 
-    // Get the number of rows on the game board from the user
-    public int numOfRows() {
-        int numOfRows;
-
-        numOfRows = Integer.parseInt(JOptionPane.showInputDialog("Please enter the number of rows you want on the board: "));
-        return numOfRows;
-    }
-
-    // Get the number of columns on the game board from the user
-    public int numOfColumns() {
+    // Ask the user for the number of columns they would like
+    public int columnDimensions() {
         int numOfColumns;
 
         numOfColumns = Integer.parseInt(JOptionPane.showInputDialog("Please enter the number of columns you want on the board: "));
         return numOfColumns;
     }
 
+    // Ask the user for the number of rows they would like
+    public int rowDimensions() {
+        int numOfRows;
+
+        numOfRows = Integer.parseInt(JOptionPane.showInputDialog("Please enter the number of rows you want on the board: "));
+        return numOfRows;
+    }
 
     // Getters and Setters
     public String getName() {
@@ -38,4 +40,23 @@ public class GameBoard {
     public void setName(String name) {
         this.name = name;
     }
+
+    // Get the number of rows from rowDimensions() input
+    public int getNumOfRows() {
+        return this.numOfRows;
+    }
+
+    // Set the numOfRows equal to rowDimensions() input
+    public void setNumOfRows(int numOfRows) {
+        this.numOfRows = numOfRows;
+    }
+
+    public int getNumOfColumns() {
+        return this.numOfColumns;
+    }
+
+    public void setNumOfColumns(int numOfColumns) {
+        this.numOfColumns = numOfColumns;
+    }
+
 }
--- PlayGame.java
+++ PlayGame.java
@@ -2,6 +2,9 @@ public class PlayGame {
     public static void main(String[] args) {
         GameBoard newGame = new GameBoard("Connect Four");
 
-
+        newGame.columnDimensions();
+        newGame.rowDimensions();
+        System.out.println(newGame.getNumOfColumns());
+        System.out.println(newGame.getNumOfRows());
     }
 }
