b1bd6e772761c775922817b8a0767c7e86cac665
Ryan <rmc7394@gmail.com>
2020-10-14 16:46:54 +0100

    Added roadmap for functions in main

--- PlayGame.java
+++ PlayGame.java
@@ -1,8 +1,19 @@
 public class PlayGame {
     public static void main(String[] args) {
+
+        // Create new game instance
         GameBoard newGame = new GameBoard("Connect Four");
 
-        System.out.println(newGame.getNumOfColumns());
-        System.out.println(newGame.getNumOfRows());
+        // Method to start the game
+        // Ask for names of players
+        // Ask player one which colour of chip they want
+        // Ask player for column number to place chip
+        // Update the space of that row/column to have a chip in it (or put it one row up if already full)
+        // OR tell the player that row is already full and ask for a different column to place
+        // Check surroundings of that chip to see if there are any matches
+        // If 4 matches in a direction are found, display winning message for player
+        // If no matches found, proceed to next players turn (Return to Ask player from column number to place chip)
+
+        // update board display after every turn?
     }
 }
