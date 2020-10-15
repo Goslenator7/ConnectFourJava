f3bdf4ddd6494192c5cf1815b2da5b08a548c82d
Ryan <rmc7394@gmail.com>
2020-10-15 14:09:49 +0100

    Adding and editing methods
    Added and edited methods in Player and PlayGame classes to test player object creation and assignment with variables.

--- PlayGame.java
+++ PlayGame.java
@@ -1,10 +1,41 @@
+import javax.swing.*;
+
 public class PlayGame {
+
+    public static void welcome() {
+        String welcome;
+
+        welcome = "Welcome to Connect 4! Players take turns dropping their chip into a column on the board. \n The first player to line up 4 of their chips in any direction wins the game!";
+        JOptionPane.showMessageDialog(null, welcome, "Welcome to Connect Four!", JOptionPane.INFORMATION_MESSAGE);
+    }
+
     public static void main(String[] args) {
+
+        PlayGame.welcome();
 
         // Create new game instance
         GameBoard newGame = new GameBoard("Connect Four");
 
-        // Method to start the game
+        Player player1 = new Player();
+        Player player2 = new Player();
+
+        player1.playerName();
+        player2.playerName();
+
+        player1.playerColour();
+
+        // Set player 2's colour to the option player 1 does not pick
+        if (player1.getPlayerColour() == 'R') {
+            player2.setPlayerColour('Y');
+        }
+        else {
+            player2.setPlayerColour('R');
+        }
+
+        String message = player1.getPlayerName()+" is "+player1.getPlayerColour()+". "+player2.getPlayerName()+" is "+player2.getPlayerColour()+".";
+        JOptionPane.showMessageDialog(null, message);
+
+        // Method to start the game //main?
         // Ask for names of players
         // Ask player one which colour of chip they want
         // Ask player for column number to place chip
--- Player.java
+++ Player.java
@@ -1,8 +1,45 @@
+import javax.swing.*;
+
 public class Player {
 
     private String playerName;
     private char playerColour;
 
+    //Constructor
+    public Player() {
+        this.getPlayerName();
+        this.getPlayerColour();
+    }
+
+    // Get the names of player one and player two
+    public void playerName() {
+        String playerName;
+
+        playerName = JOptionPane.showInputDialog("Enter player name: ");
+        setPlayerName(playerName);
+    }
+
+    // Ask the player which colour they would like to play as
+    public void playerColour() {
+        String playerColour;
+
+        playerColour = JOptionPane.showInputDialog("Okay, does "+getPlayerName()+" want to be red or yellow?");
+
+        // For some reason, it would not accept these as chained || statements, so had to do them this way
+        if (playerColour == "red") {
+            setPlayerColour('R');
+        }
+        else if (playerColour == "Red") {
+            setPlayerColour('R');
+        }
+        else if (playerColour == "R") {
+            setPlayerColour('R');
+        }
+        else {
+            setPlayerColour('Y');
+        }
+        // Can set the next player to a different colour once player objects have been created in main?
+    }
 
     // Getters and Setters
     public String getPlayerName() {
